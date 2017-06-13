d209b7a80332e8d41d01cd252ab8923d367cbb40
JJamet <jamet.jeremy@outlook.com>
2017-06-13 23:59:25 +0200

    Ajout de la traduction L10n et résolution de bugs

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,28 @@
+$(document).ready(function() {
 
-// Function that return prefered langage
-function getPreferedLanguage() {
+	//return prefered langage
 	var language;
 	if (navigator.browserLanguage)
 		language = navigator.browserLanguage;
 	else
 		language = navigator.language;
-	console.log(language);
 	switch (language) {
 		case "fr":
-			return "fr";
+			break;
 		default:
-			return "en";
+			language="en";
 	}
-}
 
-$(document).ready(function() {
+	// Change langage
+	document.webL10n.setLanguage(language);
+	language = language.substr(0,1).toUpperCase()+language.substr(1);
+	$('#language-text').text(language);
+
+	$('#menu-language a').on('click', function() {
+		language = $(this).text();
+		$('#language-text').text(language);
+		document.webL10n.setLanguage(language);
+	});
 
 	// Animation for scrolling
 	$('.scrollTo').on('click', function() { // Au clic sur un élément
@@ -23,39 +30,5 @@ $(document).ready(function() {
 		var speed = 750; // Durée de l'animation (en ms)
 		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
 		return false;
-	});
-
-	// Change language
-	$('#menu_language').on('change', function() {
-		var value = $(this).attr('value'); // Page cible
-		var pageName = window.location.pathname;
-		pageName = pageName.split("/");
-		pageName = pageName[pageName.length -1];
-
-		var splitPageName = pageName.split(".");
-		var extension = splitPageName[splitPageName.length -1];
-		if(extension.length === 0)
-			extension = "html";
-		// Remove extension from splitPageName
-		splitPageName.pop();
-		// Remove _fr
-		splitPageName = splitPageName.toString().split("_");
-		if(splitPageName.length > 1)
-			splitPageName.pop();
-		else
-			splitPageName = "index";
-		// For page in PHP
-		if(extension.toLowerCase() === "php") {
-			window.open(pageName+"?lang="+value, "_self");
-		}
-		// For page in HTML
-		else {
-			// For exemple, index.html is default page
-			var targetPage = splitPageName+"."+extension;
-			// index_fr the translation
-			if(defaultLanguage != value)
-				targetPage = splitPageName+"_"+value+"."+extension;
-			window.open(targetPage, "_self");
-		}
 	});
 });
