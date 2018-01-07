105ea2b48d73bb4cdceb3dd15dc2c765bdd6f2e0
J-Jamet <jamet.jeremy@outlook.com>
2018-01-07 14:10:33 +0100

    Clean js indentation

--- js/custom.js
+++ js/custom.js
@@ -3,178 +3,178 @@ var interval;
 var inProgress = false;
 
 $(document).ready(function() {
-	/**
-	 * Manage Langages
-	 **/
-  var _ = document.webL10n.get;
+    /**
+    * Manage Langages
+    **/
+    var _ = document.webL10n.get;
 
-	//return prefered langage
-	var language;
-	if (navigator.browserLanguage)
-		language = navigator.browserLanguage;
-	else
-		language = navigator.language;
-	switch (language) {
-		case "en":
-			break;
-		default:
-			language="fr";
-	}
+    //return prefered langage
+    var language;
+    if (navigator.browserLanguage)
+        language = navigator.browserLanguage;
+    else
+        language = navigator.language;
+    switch (language) {
+        case "en":
+                break;
+        default:
+                language="fr";
+    }
 
-	// Change langage
-	document.webL10n.setLanguage(language);
-	$('html').attr('lang', language);
-	language = language.substr(0,1).toUpperCase()+language.substr(1);
-	$('#language-text').text(language);
+    // Change langage
+    document.webL10n.setLanguage(language);
+    $('html').attr('lang', language);
+    language = language.substr(0,1).toUpperCase()+language.substr(1);
+    $('#language-text').text(language);
 
-	$('#menu-language a').on('click', function() {
-		language = $(this).text();
-		$('#language-text').text(language);
-		$('html').attr('lang', language.substr(0,1).toLowerCase()+language.substr(1));
-		document.webL10n.setLanguage(language);
-	});
+    $('#menu-language a').on('click', function() {
+        language = $(this).text();
+        $('#language-text').text(language);
+        $('html').attr('lang', language.substr(0,1).toLowerCase()+language.substr(1));
+        document.webL10n.setLanguage(language);
+    });
 
-	// Tabs Table
-	var hideColumn = function(tabTh) {
-		if(!tabTh.hasClass('col-active')) {
-			console.log(tabTh);
-			$.each($('.tab-col'), function(keyTab, nodeTab) {
-				$($(nodeTab).data('col')).addClass('col-hidden');
-				$(nodeTab).removeClass('col-active');
-			});
+    // Tabs Table
+    var hideColumn = function(tabTh) {
+        if(!tabTh.hasClass('col-active')) {
+            console.log(tabTh);
+            $.each($('.tab-col'), function(keyTab, nodeTab) {
+                    $($(nodeTab).data('col')).addClass('col-hidden');
+                    $(nodeTab).removeClass('col-active');
+            });
 
-			initHiddenClick();
+            initHiddenClick();
 
-			$(tabTh.data('col')).removeClass('col-hidden');
-			tabTh.addClass('col-active');
-		}
-	}
+            $(tabTh.data('col')).removeClass('col-hidden');
+            tabTh.addClass('col-active');
+        }
+    }
 
-	// For each click on hidden element
-	var initHiddenClick = function() {
-		$('.col-hidden').bind('click.kunzisoftEvent', function() {
-			var dataColumn = $(this).data('col');
-			var tab = $('.tab-col').filter('[data-col="'+dataColumn+'"]');
-			hideColumn(tab);
-		});
-	}
+    // For each click on hidden element
+    var initHiddenClick = function() {
+        $('.col-hidden').bind('click.kunzisoftEvent', function() {
+            var dataColumn = $(this).data('col');
+            var tab = $('.tab-col').filter('[data-col="'+dataColumn+'"]');
+            hideColumn(tab);
+        });
+    }
 
-	var tabTableInit = function() {
-		if($( window ).width() < 600) {
-			// Init hidden elements
-			$.each($('.tab-col'), function(keyTab, nodeTab) {
-				if(!$(this).hasClass('col-active')) {
-					$($(this).data('col')).addClass('col-hidden');
-				}
-			});
+    var tabTableInit = function() {
+        if($( window ).width() < 600) {
+            // Init hidden elements
+            $.each($('.tab-col'), function(keyTab, nodeTab) {
+                    if(!$(this).hasClass('col-active')) {
+                            $($(this).data('col')).addClass('col-hidden');
+                    }
+            });
 
-			// For each click on tab
-			$('.tab-col').bind('click.kunzisoftEvent', function() {
-				hideColumn($(this));
-			});
+            // For each click on tab
+            $('.tab-col').bind('click.kunzisoftEvent', function() {
+                    hideColumn($(this));
+            });
 
-			initHiddenClick();
-		} else {
-			$('.col-hidden').unbind('click.kunzisoftEvent');
-			$('.col-hidden').removeClass('col-hidden');
-			$('.tab-col').unbind('click.kunzisoftEvent');
-		}
-	}
+            initHiddenClick();
+        } else {
+            $('.col-hidden').unbind('click.kunzisoftEvent');
+            $('.col-hidden').removeClass('col-hidden');
+            $('.tab-col').unbind('click.kunzisoftEvent');
+        }
+    }
 
-	// Color projects
-	$('.project').css('background', function( index ) {
-		if($(this).data('color-gradient') != undefined) {
-			return 'linear-gradient('+$(this).data('color')+', '+$(this).data('color-gradient')+')';
-		} else
-	  	return $(this).data('color');
-	});
+    // Color projects
+    $('.project').css('background', function( index ) {
+        if($(this).data('color-gradient') != undefined) {
+                return 'linear-gradient('+$(this).data('color')+', '+$(this).data('color-gradient')+')';
+        } else
+        return $(this).data('color');
+    });
 
-	// Animation for scrolling
-	$('.scrollTo').on('click', function() { // Au clic sur un élément
-		var page = $(this).attr('href'); // Page cible
-		var speed = 750; // Durée de l'animation (en ms)
-		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-		return false;
-	});
+    // Animation for scrolling
+    $('.scrollTo').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
 
-	// Send quote mail
-	$('.quote-email').on('click', function() {
+    // Send quote mail
+    $('.quote-email').on('click', function() {
     var formula = $('input:radio[name=formula]:checked').val();
-		$(location).attr('href', 'mailto:'+$(this).attr('data-email')+'?subject='
-                             + encodeURIComponent(_('emailSubject', { formula: formula }))
-                             + "&body="
-                             + encodeURIComponent(_('emailBody'))
-                           );
-	});
+        $(location).attr('href', 'mailto:'+$(this).attr('data-email')+'?subject='
+            + encodeURIComponent(_('emailSubject', { formula: formula }))
+            + "&body="
+            + encodeURIComponent(_('emailBody'))
+        );
+});
 
-  var visibleNavbar = function() {
+var visibleNavbar = function() {
     $('.navbar').removeClass("navbar-transparent");
     $('.navbar').addClass("navbar-default");
-  }
+}
 
-  var transparentNavbar = function() {
+var transparentNavbar = function() {
     $('.navbar').addClass("navbar-transparent");
     $('.navbar').removeClass("navbar-default");
-  }
+}
 
-	/**
-	 * Listen to scroll to change header opacity class
-	 */
-	function checkScroll() {
-	    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
-      if($('.navbar-toggle').attr('aria-expanded') == "true") {
-        visibleNavbar();
-      } else if($(window).scrollTop() <= startY){
-        transparentNavbar();
-	    } else{
-        visibleNavbar();
-	    }
-	}
-
-  $('.navbar-toggle').on("click", function(){
+        /**
+        * Listen to scroll to change header opacity class
+        */
+        function checkScroll() {
+            var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
     if($('.navbar-toggle').attr('aria-expanded') == "true") {
-      transparentNavbar();
+        visibleNavbar();
+    } else if($(window).scrollTop() <= startY){
+        transparentNavbar();
+            } else{
+        visibleNavbar();
+            }
+        }
+
+$('.navbar-toggle').on("click", function(){
+    if($('.navbar-toggle').attr('aria-expanded') == "true") {
+    transparentNavbar();
     } else {
-      visibleNavbar();
+    visibleNavbar();
     }
-  });
+});
 
-  var startTopAnimation = function() {
+var startTopAnimation = function() {
     if(!inProgress) {
-      buildDotsCanvas();
+    buildDotsCanvas();
     }
-  }
+}
 
-  var stopTopAnimation = function() {
+var stopTopAnimation = function() {
     inProgress = false;
     clearInterval(interval);
-  }
+}
 
-  var resizeWindowEnd = function() {
+var resizeWindowEnd = function() {
     startTopAnimation();
-		tabTableInit();
-  }
+                tabTableInit();
+}
 
-  var isResizing;
-  $(window).on("load resize", function() {
+var isResizing;
+$(window).on("load resize", function() {
     stopTopAnimation();
     clearTimeout(isResizing);
     isResizing = setTimeout(resizeWindowEnd, 100);
-  });
+});
 
-  $(window).on("scroll", function() {
+$(window).on("scroll", function() {
     if ($(window).scrollTop() > $('#tophome').height()) {
-      stopTopAnimation();
+    stopTopAnimation();
     } else {
-      startTopAnimation();
+    startTopAnimation();
     }
-  });
+});
 
-	if($('.navbar').length > 0){
-			$(window).on("scroll load resize", function(){
-					checkScroll();
-			});
-	}
+        if($('.navbar').length > 0){
+                        $(window).on("scroll load resize", function(){
+                                        checkScroll();
+                        });
+        }
 });
 
 var buildDotsCanvas = function() {
